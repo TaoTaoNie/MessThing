4240d6a240045ffa1bdb9f4843e7759715692c15
leetHuam <980321356@qq.com>
2019-01-27 21:32:45 +0800

    change3

--- 6354-3191/src/Counter.java
+++ 6354-3191/src/Counter.java
@@ -6,8 +6,7 @@
  * @author Charles Hoot
  * @version 4.0
  */
-public class Counter
-{
+public class Counter {
     // PUT PRIVATE DATA FIELDS HERE
     private int minValue;
     private int maxValue;
@@ -18,8 +17,7 @@ public class Counter
      * The default constructor for objects of class Counter.  Minimum is 0 and the maximum
      * is the largest possible integer.
      */
-    public Counter()
-    {
+    public Counter() {
         // ADD CODE FOR THE CONSTRUCTOR
         currentValue = minValue = 0;
         maxValue = Integer.MAX_VALUE;
@@ -29,11 +27,11 @@ public class Counter
     /**
      * The alternate constructor for objects of class Counter.  The minimum and maximum values are given as parameters.
      * The counter starts at the minimum value.
+     *
      * @param min The minimum value that the counter can have
      * @param max The maximum value that the counter can have
-     * */
-    public Counter(int min, int max)
-    {
+     */
+    public Counter(int min, int max) {
         // ADD CODE FOR THE ALTERNATE CONSTRUCTOR
         if (min == max)
             throw new CounterInitializationException("min == max");
@@ -46,16 +44,14 @@ public class Counter
     /**
      * Determine if two counters are in the same state
      *
-     * @param  otherObject   the object to test against for equality
-     * @return     true if the objects are in the same state
+     * @param otherObject the object to test against for equality
+     * @return true if the objects are in the same state
      */
-    public boolean equals(Object otherObject)
-    {
+    public boolean equals(Object otherObject) {
         boolean result = true;
-        if (otherObject instanceof Counter)
-        {
+        if (otherObject instanceof Counter) {
             // YOUR CODE GOES HERE
-            Counter other = (Counter)otherObject;
+            Counter other = (Counter) otherObject;
             if (minValue != other.minValue || maxValue != other.maxValue || currentValue != other.currentValue || rolledOver != other.rolledOver)
                 result = false;
         }
@@ -63,18 +59,15 @@ public class Counter
     }
 
 
-
     /**
      * Increases the counter by one
      */
-    public void increase()
-    {
+    public void increase() {
         // ADD CODE TO INCREASE THE VALUE OF THE COUNTER
         if (currentValue == maxValue) {
             currentValue = minValue;
             rolledOver = true;
-        }
-        else {
+        } else {
             currentValue++;
             rolledOver = false;
         }
@@ -84,14 +77,12 @@ public class Counter
     /**
      * Decreases the counter by one
      */
-    public void decrease()
-    {
+    public void decrease() {
         // ADD CODE TO INCREASE THE VALUE OF THE COUNTER
-        if (currentValue == minValue){
+        if (currentValue == minValue) {
             currentValue = maxValue;
             rolledOver = true;
-        }
-        else {
+        } else {
             currentValue--;
             rolledOver = false;
         }
@@ -100,10 +91,9 @@ public class Counter
     /**
      * Get the value of the counter
      *
-     * @return     the current value of the counter
+     * @return the current value of the counter
      */
-    public int value()
-    {
+    public int value() {
         // CHANGE THE RETURN TO GIVE THE CURRENT VALUE OF THE COUNTER
         return currentValue;
 
@@ -112,12 +102,11 @@ public class Counter
 
     /**
      * Accessor that allows the client to determine if the counter
-     *             rolled over on the last count
+     * rolled over on the last count
      *
-     * @return     true if the counter rolled over
+     * @return true if the counter rolled over
      */
-    public boolean rolledOver()
-    {
+    public boolean rolledOver() {
         // CHANGE THE RETURN TO THE ROLLOVER STATUS OF THE COUNTER
         return rolledOver;
     }
@@ -126,10 +115,9 @@ public class Counter
      * Override the toString method to provide a more informative
      * description of the counter
      *
-     * @return     a descriptive string about the object
+     * @return a descriptive string about the object
      */
-    public String toString()
-    {
+    public String toString() {
         // CHANGE THE RETURN TO A DESCRIPTION OF THE COUNTER
         return "max: " + this.maxValue + " min: " + this.minValue + " current value: " + this.currentValue;
     }
